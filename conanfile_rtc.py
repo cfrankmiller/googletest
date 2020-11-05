f03a698eab3f824adb051a5c9ce549e30c631861
Christian Venegas <cvenegas@esri.com>
2020-11-04 16:37:57 -0800

    build: Add a conanfile to package artifacts (#9)

---
+++ conanfile_rtc.py
@@ -0,0 +1,24 @@
+from conans import ConanFile
+
+
+class GoogletestConan(ConanFile):
+    name = "googletest"
+    version = "1.10.x"
+    url = "https://github.com/Esri/googletest/tree/runtimecore"
+    license = "https://github.com/Esri/googletest/blob/runtimecore/LICENSE"
+    description = "Googletest - Google Testing and Mocking Framework."
+
+    # RTC specific triple
+    settings = "platform_architecture_target"
+
+    def package(self):
+        base = self.source_folder + "/"
+        relative = "3rdparty/googletest/"
+
+        # headers
+        self.copy("*.h*", src=base + "googlemock/include", dst=relative + "googlemock/include", excludes="*.pump")
+        self.copy("*.h*", src=base + "googletest/include", dst=relative + "googletest/include")
+
+        # libraries
+        output = "output/" + str(self.settings.platform_architecture_target) + "/staticlib"
+        self.copy("*" + self.name + "*", src=base + "../../" + output, dst=output)
